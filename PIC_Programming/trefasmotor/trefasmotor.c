8c4f5e2e10a140bdfb3520c4d1d3eebc16458f3f
AntonHakansson <anton.hakansson98@gmail.com>
2017-03-16 16:22:42 +0100

    Started answering assignment questions

--- PIC_Programming/trefasmotor/trefasmotor.c
+++ PIC_Programming/trefasmotor/trefasmotor.c
@@ -1,9 +1,41 @@
 /*
-  - Dimensionering av transistor-switchen
+--- Dimensionering av transistor-switchen
 
-    Potentialskillnaden mellan emittorn och kollektorn är 5v där emittorn går
-    till jord och på så vis är mer negativ än kollektorn
+    För att NPN transistorn ska leda måste två villkor uppfyllas
+      1. Emittorn ska vara mer positiv än kollektorn
+      2. Potentialskillnaden mellan basen och kollektorn ska vara ~0.6V
 
+    Första villkoret är uppfyllt där Emittorn
+
+    För att uppfylla andra villkoret så vill vi mata transistorns bas med 0.6V
+    och vill därför ha ett spänningfall från PIC-utgången som ligger 5V.
+    Spänningsfallet vi söker blir således 5.0 - 0.6 = 4.4V.
+    Vi tilldelar ett rimligt värde för strömmen och låter:
+      I = 2mA
+    Vi får att:
+      R = U/I = 4.4/0.02 = 220 ohm
+
+    Vi avrundar upp resistansen till 330 ohm vilket finns tillgängligt i
+    laborationssalen.
+
+--- Vad som är viktigt att kontrollera när man väljer ett relä till en viss applikation
+
+    Den absolut viktigaste tekniska specificationen är ett reläs brytförmåga.
+    Brytförmågan beskriver den maximal lasten man kan ha under brytningstillfället.
+    Om man överstiger denna gräns finns risken att en ljusbåge bildas som i längden
+    bränner sönder kontaktpunkterna eller under extrema fall svetsar fast
+    kontaktbläcken.
+
+--- En liten förklaring av trefasmotorns funktion
+
+    Den grundläggande principen bygger på nätets tre faser som förskjutna 120
+    grader sinns i mellan. Faserna ansluts till spolar statorn.
+    I trefasmotorns rotor går rotorns ledarräls. Då dessa utsätts för statorns
+    varierande magnetfält induceras en spänning som driver ström genom rotorledarna.
+    Den inducerade spänningen ger upphov till ett magnetfält som fångas upp av
+    rotorns roterande magnetfält.
+
+    se http://28oa9i1t08037ue3m1l0i861.wpengine.netdna-cdn.com/wp-content/uploads/2015/05/inductionmotoranimation.gif
 */
 
 // =============================================================================
